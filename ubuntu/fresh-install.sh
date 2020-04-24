67151831285c043fd0042f246eee81007a1da60c
Nicholas Nadeau <nicholas.nadeau@gmail.com>
2020-04-24 08:54:38 -0400

    added scripts

---
+++ ubuntu/fresh-install.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+sudo apt install -y \
+    clamav \
+    cmake \
+    copyq \
+    cppcheck \
+    curl \
+    doxygen \
+    exfat-fuse \
+    exfat-utils \
+    flameshot \
+    fonts-powerline \
+    gimp \
+    git \
+    gitk \
+    gnome-tweaks \
+    graphviz \
+    handbrake \
+    htop \
+    imagemagick \
+    inkscape \
+    jpegoptim \
+    latexmk \
+    libreoffice-calc \
+    libreoffice-writer \
+    make \
+    meshlab \
+    ncdu \
+    okular \
+    pandoc-citeproc \
+    pulseaudio \
+    texlive \
+    texlive-fonts-extra \
+    texlive-full \
+    texlive-xetex \
+    tree \
+    vim \
+    wget \
+    zsh
+
+sudo snap install --classic code
+sudo snap install --classic slack
+sudo snap install spotify
+sudo snap install vlc
+sudo snap install okular
