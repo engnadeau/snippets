7a634fa6bc848a0e526ec42c7b754c52d48216a9
Nicholas Nadeau <nicholas.nadeau@gmail.com>
2020-07-09 08:11:13 -0400

    added ability to copy to clipboard

--- python/webvtt_to_text.py
+++ python/webvtt_to_text.py
@@ -2,12 +2,14 @@
 
 import logging
 from pathlib import Path
+from typing import Optional
 
 import fire
+import pyperclip
 import webvtt
 
 
-def main(path: str, output: str):
+def main(path: str, output: Optional[str] = None, copy: bool = False):
     path = Path(path)
     logging.info(f"Processing: {path.resolve()}")
 
@@ -15,10 +17,15 @@ def main(path: str, output: str):
     captions = [c.text for c in captions]
     captions = " ".join(captions)
 
-    output = Path(output)
-    logging.info(f"Writing to output: {output.resolve()}")
-    with open(output, "w") as f:
-        f.write(captions)
+    if output:
+        output = Path(output)
+        logging.info(f"Writing to output: {output.resolve()}")
+        with open(output, "w") as f:
+            f.write(captions)
+
+    if copy:
+        logging.info("Captions copied to clipboard")
+        pyperclip.copy(captions)
 
 
 if __name__ == "__main__":
